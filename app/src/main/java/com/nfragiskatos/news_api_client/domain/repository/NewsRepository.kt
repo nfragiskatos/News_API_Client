c4e95575b36ccdf65fec6a4af1968ce5a108408a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-09 11:09:08 -0700

    - added coroutines dependency to app gradle - Added initial set of function definitions to NewsRepository.kt

--- app/src/main/java/com/nfragiskatos/news_api_client/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/nfragiskatos/news_api_client/domain/repository/NewsRepository.kt
@@ -1,4 +1,17 @@
 package com.nfragiskatos.news_api_client.domain.repository
 
+import com.nfragiskatos.news_api_client.data.model.APIResponse
+import com.nfragiskatos.news_api_client.data.model.Article
+import com.nfragiskatos.news_api_client.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+
 interface NewsRepository {
+
+    suspend fun getNewsHeadlines() : Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String) : Resource<APIResponse>
+
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews() : Flow<List<Article>>
 }
