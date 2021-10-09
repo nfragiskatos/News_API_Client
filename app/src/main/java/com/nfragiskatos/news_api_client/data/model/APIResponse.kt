71d53d8f00808a7ddddd14ab559f4ab626b7606a
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-09 10:57:05 -0700

    - Added Gson dependency to app gradle - Created data models for the API response given by the news API

---
+++ app/src/main/java/com/nfragiskatos/news_api_client/data/model/APIResponse.kt
@@ -0,0 +1,13 @@
+package com.nfragiskatos.news_api_client.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class APIResponse(
+    @SerializedName("articles")
+    val articles: List<Article>,
+    @SerializedName("status")
+    val status: String,
+    @SerializedName("totalResults")
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/nfragiskatos/news_api_client/data/model/Source.kt
@@ -0,0 +1,11 @@
+package com.nfragiskatos.news_api_client.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: Any,
+    @SerializedName("name")
+    val name: String
+)
