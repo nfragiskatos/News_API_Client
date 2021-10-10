cb9b07dea4c8d219d232a6b902d493b628263b76
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-10 10:54:16 -0700

    - added bottom menu with 2 buttons to navigate to the NewsFragment and SavedFragment

--- app/src/main/java/com/nfragiskatos/news_api_client/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/news_api_client/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nfragiskatos.news_api_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.nfragiskatos.news_api_client.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
