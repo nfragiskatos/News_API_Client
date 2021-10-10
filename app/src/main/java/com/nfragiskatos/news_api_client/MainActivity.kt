36ce3459ce38974cb92b78639f2e80a508fb2d3c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-10 08:12:07 -0700

    - added dependencies for safeargs and jetpack navigation

--- app/src/main/java/com/nfragiskatos/news_api_client/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/news_api_client/MainActivity.kt
@@ -2,10 +2,15 @@ package com.nfragiskatos.news_api_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nfragiskatos.news_api_client.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
