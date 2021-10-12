9e4af5881343c94b00eea4aa31cc5c975f8b97ea
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-11 20:29:10 -0700

    - setup NewsFragment.kt to fetch news headlines and display them in the recycler view

--- app/src/main/java/com/nfragiskatos/news_api_client/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/news_api_client/MainActivity.kt
@@ -2,13 +2,24 @@ package com.nfragiskatos.news_api_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.nfragiskatos.news_api_client.databinding.ActivityMainBinding
+import com.nfragiskatos.news_api_client.presentation.viewmodel.NewsViewModel
+import com.nfragiskatos.news_api_client.presentation.viewmodel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +28,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+        viewModel = ViewModelProvider(this, factory)[NewsViewModel::class.java]
     }
 }
