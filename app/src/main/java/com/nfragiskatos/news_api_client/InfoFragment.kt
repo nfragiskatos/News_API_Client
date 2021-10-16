8a90314b366b3b5be02dba26c2a8f25bc99adc51
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-16 06:36:53 -0700

    - Added FloatingActionBar to fragment_info.xml to allow user to trigger a save of the news article - In InfoFragment.kt implemented onClick for FAB to trigger the save via the NewsViewModel

--- app/src/main/java/com/nfragiskatos/news_api_client/InfoFragment.kt
+++ app/src/main/java/com/nfragiskatos/news_api_client/InfoFragment.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebViewClient
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.nfragiskatos.news_api_client.databinding.FragmentInfoBinding
+import com.nfragiskatos.news_api_client.presentation.viewmodel.NewsViewModel
 
 
 class InfoFragment : Fragment() {
 
     private lateinit var fragmentInfoBinding: FragmentInfoBinding
+    private lateinit var newsViewModel: NewsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +28,7 @@ class InfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fragmentInfoBinding = FragmentInfoBinding.bind(view)
+        newsViewModel = (activity as MainActivity).viewModel
         val args: InfoFragmentArgs by navArgs()
         val article = args.selectedArticle
         fragmentInfoBinding.wvInfo.apply {
@@ -32,6 +36,10 @@ class InfoFragment : Fragment() {
             if (!article.url.isNullOrBlank()) {
                 loadUrl(article.url)
             }
+        }
+        fragmentInfoBinding.fabSave.setOnClickListener {
+            newsViewModel.saveArticle(article)
+            Snackbar.make(view, "Save Successful", Snackbar.LENGTH_LONG).show()
         }
     }
 }
