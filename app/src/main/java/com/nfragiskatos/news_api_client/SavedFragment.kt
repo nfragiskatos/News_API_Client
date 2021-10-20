2e7b4af5779ffb98ea96ada4f7555c8d472e18d4
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-10-19 20:28:04 -0700

    - partially migrated over the saved list screen. Need to implement the swipe to delete, as well as the navigation to view the news article info screen when item is clicked

--- app/src/main/java/com/nfragiskatos/news_api_client/SavedFragment.kt
+++ app/src/main/java/com/nfragiskatos/news_api_client/SavedFragment.kt
@@ -1,18 +1,33 @@
 package com.nfragiskatos.news_api_client
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import com.nfragiskatos.news_api_client.data.model.Article
 import com.nfragiskatos.news_api_client.databinding.FragmentSavedBinding
 import com.nfragiskatos.news_api_client.presentation.adapter.NewsAdapter
+import com.nfragiskatos.news_api_client.presentation.compose.articlelist.ArticleListItem
 import com.nfragiskatos.news_api_client.presentation.viewmodel.NewsViewModel
+import java.util.*
 
 
 class SavedFragment : Fragment() {
@@ -21,32 +36,37 @@ class SavedFragment : Fragment() {
     private lateinit var fragmentSavedBinding: FragmentSavedBinding
     private lateinit var newsAdapter: NewsAdapter
 
+    private lateinit var binding: FragmentSavedBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saved, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragmentSavedBinding = FragmentSavedBinding.bind(view)
+        binding = FragmentSavedBinding.bind(view)
         viewModel = (activity as MainActivity).viewModel
-        newsAdapter = (activity as MainActivity).newsAdapter
-        newsAdapter.setOnItemClickListener {
-            val bundle = Bundle().apply {
-                putSerializable("selected_article", it)
+//        newsAdapter = (activity as MainActivity).newsAdapter
+//        newsAdapter.setOnItemClickListener {
+//            val bundle = Bundle().apply {
+//                putSerializable("selected_article", it)
+//            }
+//            findNavController().navigate(
+//                R.id.action_savedFragment_to_infoFragment,
+//                bundle
+//            )
+//        }
+//        initRecyclerView()
+        binding.savedComposeView.setContent {
+            MaterialTheme() {
+                DisplaySavedArticles(viewModel = viewModel)
             }
-            findNavController().navigate(
-                R.id.action_savedFragment_to_infoFragment,
-                bundle
-            )
         }
-        initRecyclerView()
-        viewModel.getSavedNews().observe(viewLifecycleOwner, {
-            newsAdapter.differ.submitList(it)
-        })
 
         val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
             ItemTouchHelper.UP or ItemTouchHelper.DOWN,
@@ -73,16 +93,42 @@ class SavedFragment : Fragment() {
             }
         }
 
-        ItemTouchHelper(itemTouchHelperCallback).apply {
-            attachToRecyclerView(fragmentSavedBinding.rvSaved)
+//        ItemTouchHelper(itemTouchHelperCallback).apply {
+//            attachToRecyclerView(fragmentSavedBinding.rvSaved)
+//        }
+    }
+
+    @Composable
+    private fun DisplaySavedArticles(articles: List<Article>) {
+        LazyColumn(contentPadding = PaddingValues(horizontal = 4.dp, vertical = 4.dp)) {
+            items(
+                items = articles,
+                itemContent = {
+                    ArticleListItem(article = it)
+                }
+            )
         }
     }
 
-    private fun initRecyclerView() {
-        fragmentSavedBinding.rvSaved.apply {
-            adapter = newsAdapter
-            layoutManager = LinearLayoutManager(activity)
+    @Composable
+    private fun DisplaySavedArticles(viewModel: NewsViewModel) {
+        val articles by viewModel.getSavedNews().observeAsState()
+
+        if (articles == null) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        } else {
+            LazyColumn(contentPadding = PaddingValues(horizontal = 4.dp, vertical = 4.dp)) {
+                items(
+                    items = articles!!,
+                    itemContent = {
+                        ArticleListItem(article = it)
+                    }
+                )
+            }
         }
     }
-
 }
